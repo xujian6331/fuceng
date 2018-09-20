24db696a7bc8253e938519a3a56874bdf7998946
xujian6331 <1158697126Qqq.com>
2018-09-20 13:16:52 +0800

    add

---
+++ fc.js
@@ -0,0 +1,20 @@
+
+$(clickMe).on('click', function() {
+  $(popover).show()
+  console.log('show')
+  setTimeout(function() {
+    console.log('添加 one click')
+    $(document).one('click', function() {
+      console.log('我觉得这里不会执行')
+      console.log('hide')
+      $(popover).hide()
+    })
+  }, 0)
+
+})
+$('#wrapper').on('click',function(e){
+	e.stopPropagation()
+})
+$(document).on(click,function(){
+	$('popover').hide()
+})
